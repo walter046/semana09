ad262172dbf25152d5b1c1ff3e991ba8badf9e19
Walter PL <u22241414@utp.edu.pe>
2025-05-22 11:21:56 -0500

    Add another test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 const { add } = require('./index');
 
 test('adds 1 + 2 to equal 3' , () => {
-    expect(add(1, 2)).tobe(3);
+    expect(add(1, 2)).toBe(3);
 });
 
 test('adds 5 + 7 to equal 12' , () => {
