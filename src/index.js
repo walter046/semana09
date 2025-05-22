f5c3d56fd01847cb300512bcc9d73fecefbb5187
Walter PL <u22241414@utp.edu.pe>
2025-05-22 11:35:05 -0500

    Add another test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function add(a, b, c, d) {
-    return a+b * (c-d);
+    return (a+b) * (c-d)
 }
 
 module.exports = {add};
--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,9 @@
 const { add } = require('./index');
 
-test('adds 1 + 2 * (5-3) to equal 6' , () => {
+test('adds (1 + 2) * (5-3) to equal 6' , () => {
     expect(add(1, 2, 5, 3)).toBe(6);
 });
 
-test('adds 5 + 7 * (10-5) to equal 60' , () => {
+test('adds (5 + 7) * (10-5) to equal 60' , () => {
     expect(add(5, 7, 10, 5)).toBe(60);
 });
